9e8d3b843a7326a3a7b6accbcea552096ec12bac
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-12-19 02:11:54 +0000

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
-import { Hono } from "hono";
+import { Hono,Env as henv } from "hono";
 
-const app = new Hono();
+const BEARER="bearer "
+const GET_FEED_SKELETON="/xrpc/app.bsky.feed.getFeedSkeleton"
 
-app.get("/xrpc/", (c) => {
+interface Env{
+	Bindings:CloudflareBindings,Variables:{did:string}
+}
+const app = new Hono<Env>();
+
+app.get(GET_FEED_SKELETON, async(c) => {
+	const auth=c.req.header("Authorization")
+	if(!auth||!auth.startsWith(BEARER)){
+		return c.json({name:"Unauthorized"},{status:401})
+	}
+	const token=auth.slice(BEARER.length)
+	const did = JSON.parse(atob(token.split(".")[1])).iss;
+	c.set("did",did)
+
 	return c.text("hello xrpc");
 });
+
+app.onError((err,c)=>{
+	console.error(err)
+	return c.json({name:"UnknownError"},500)
+})
 
 export default app;
